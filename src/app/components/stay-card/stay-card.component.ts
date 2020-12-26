6380d74647a51d595907c1ca5561b25c5405d069
daisuke0818 <faculty.miyakoda@gmail.com>
2020-12-26 17:22:29 +0900

    フォントの設定などを実施。

--- src/app/components/stay-card/stay-card.component.ts
+++ src/app/components/stay-card/stay-card.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+// 宿泊先のデータ
+import { data } from '../../stay-data/stay-data';
 
 @Component({
   selector: 'app-stay-card',
   templateUrl: './stay-card.component.html',
-  styleUrls: ['./stay-card.component.scss']
+  styleUrls: ['./stay-card.component.scss'],
 })
 export class StayCardComponent implements OnInit {
+  accData = data;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/container/stay-list/stay-list.component.ts
+++ src/app/container/stay-list/stay-list.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stay-list',
   templateUrl: './stay-list.component.html',
-  styleUrls: ['./stay-list.component.scss']
+  styleUrls: ['./stay-list.component.scss'],
 })
 export class StayListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
