b450d1cb8ab0acd7b9db19321f3e90e8105d18ea
daisuke0818 <faculty.miyakoda@gmail.com>
2020-12-28 17:56:59 +0900

    angularをver11.0にした🎉

--- src/app/container/stay-list/stay-list.component.spec.ts
+++ src/app/container/stay-list/stay-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StayListComponent } from './stay-list.component';
 
@@ -6,7 +6,7 @@ describe('StayListComponent', () => {
   let component: StayListComponent;
   let fixture: ComponentFixture<StayListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StayListComponent ]
     })
