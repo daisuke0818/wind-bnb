ad2a98ab0f12166433416f1eea8faa8e356710ae
daisuke0818 <faculty.miyakoda@gmail.com>
2020-12-28 17:48:54 +0900

    NgRxをインストールしたよ🎉

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { SearchDialogComponent } from './components/search-dialog/search-dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 
 @NgModule({
   declarations: [AppComponent, StayListComponent, StayCardComponent, SearchBoxComponent, SearchDialogComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule, FlexLayoutModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, FlexLayoutModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
