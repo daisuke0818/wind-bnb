f6facef84369166dec6602b878efc33d5cf448a2
daisuke0818 <18fs118e@shinshu-u.ac.jp>
2021-05-02 17:44:49 +0900

    npmのインストールを実施, routerの設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StayListComponent } from './container/stay-list/stay-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StayListComponent },
+  { path: '**', component: StayListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
